08ee863ba07edb4fefd862bb8cb38e2ce625ce41
cheyenneabel <113124769+cheyenneabel@users.noreply.github.com>
2023-05-03 14:20:07 -0400

    initial project upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Jacob Psenicka</h1>
+      
+      <img src="/images/soft_reset.jpeg" alt="landing page image" />
+     
+    </>
+  )
+}
+
+export default App
